7d1fedf12c39099c99341154a1838630353ad0f2
Yuen Hsi <yuenhsi93@gmail.com>
2016-11-26 14:05:50 -0800

    add onClickListeners, resource files, link buttons

--- app/src/main/java/com/bignerdranch/popquiz/MainActivity.java
+++ app/src/main/java/com/bignerdranch/popquiz/MainActivity.java
@@ -2,12 +2,33 @@ package com.bignerdranch.popquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mFalseButton = (Button)findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: implement me!
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: implement me!
+            }
+        });
     }
 }
